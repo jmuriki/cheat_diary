0f28bc52508bddb57761e307756353ef7fd0a93e
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-12-16 11:52:18 +0300

    Use Model.Exception & more
    - Delete useless imports
    - Abolish choose_commendation
    - Add raise to get_schoolkid

--- cheat.py
+++ cheat.py
@@ -1,8 +1,41 @@
 import random
 
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from datacenter.models import (Schoolkid, Mark, Chastisement,
                                 Lesson, Subject, Commendation, Teacher)
+
+
+COMMENDATIONS = [
+    "Молодец!",
+    "Отлично!",
+    "Хорошо!",
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!",
+    "Великолепно!",
+    "Прекрасно!",
+    "Ты меня очень обрадовал!",
+    "Именно этого я давно ждал от тебя!",
+    "Сказано здорово – просто и ясно!",
+    "Ты, как всегда, точен!",
+    "Очень хороший ответ!",
+    "Талантливо!",
+    "Ты сегодня прыгнул выше головы!",
+    "Я поражен!",
+    "Уже существенно лучше!",
+    "Потрясающе!",
+    "Замечательно!",
+    "Прекрасное начало!",
+    "Так держать!",
+    "Ты на верном пути!",
+    "Здорово!",
+    "Это как раз то, что нужно!",
+    "Я тобой горжусь!",
+    "С каждым разом у тебя получается всё лучше!",
+    "Мы с тобой не зря поработали!",
+    "Я вижу, как ты стараешься!",
+    "Ты растешь над собой!",
+    "Ты многое сделал, я это вижу!",
+    "Теперь у тебя точно все получится!",
+]
 
 
 def fix_marks(schoolkid, bad_marks=[2, 3], good_mark=5):
@@ -17,46 +50,10 @@ def choose_subject_title(schoolkid):
     return random.choice(Subject.objects.filter(year_of_study=schoolkid.year_of_study)).title
 
 
-def choose_commendation():
-    commendations = [
-        "Молодец!",
-        "Отлично!",
-        "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!",
-        "Великолепно!",
-        "Прекрасно!",
-        "Ты меня очень обрадовал!",
-        "Именно этого я давно ждал от тебя!",
-        "Сказано здорово – просто и ясно!",
-        "Ты, как всегда, точен!",
-        "Очень хороший ответ!",
-        "Талантливо!",
-        "Ты сегодня прыгнул выше головы!",
-        "Я поражен!",
-        "Уже существенно лучше!",
-        "Потрясающе!",
-        "Замечательно!",
-        "Прекрасное начало!",
-        "Так держать!",
-        "Ты на верном пути!",
-        "Здорово!",
-        "Это как раз то, что нужно!",
-        "Я тобой горжусь!",
-        "С каждым разом у тебя получается всё лучше!",
-        "Мы с тобой не зря поработали!",
-        "Я вижу, как ты стараешься!",
-        "Ты растешь над собой!",
-        "Ты многое сделал, я это вижу!",
-        "Теперь у тебя точно все получится!",
-    ]
-    return random.choice(commendations)
-
-
 def create_commendation(schoolkid, subject_title=None):
+    text = random.choice(COMMENDATIONS)
     if not subject_title:
         subject_title = choose_subject_title(schoolkid)
-    text = choose_commendation()
     subjects = Subject.objects.filter(
                     title__contains=subject_title,
                     year_of_study=schoolkid.year_of_study,
@@ -87,10 +84,12 @@ def get_schoolkid(name="Фролов Иван Григорьевич"):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=name)
         return schoolkid
-    except ObjectDoesNotExist:
-        print("Совпадений не найдено! Уточните ФИО ученика.")
-    except MultipleObjectsReturned:
-        print("Найдено более одного совпадения! Уточните ФИО ученика.")
+    except Schoolkid.DoesNotExist:
+        print("\n\n\n\n\nСовпадений не найдено! Уточните ФИО ученика.\n\n\n\n\n")
+        raise
+    except Schoolkid.MultipleObjectsReturned:
+        print("\n\n\n\n\nНайдено более одного совпадения! Уточните ФИО ученика.\n\n\n\n\n")
+        raise
 
 
 def main():
